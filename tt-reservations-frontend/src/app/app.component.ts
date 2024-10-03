f13d50c439d71937d08c709523f3b8a70d2b1c53
Kory291 <01lukasschaefer@gmail.com>
2024-10-03 16:42:07 +0200

    add form with start and end time

--- tt-reservations-frontend/src/app/app.component.ts
+++ tt-reservations-frontend/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 import { TimePickerComponent } from './time-picker/time-picker.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule, TimePickerComponent],
+  imports: [RouterOutlet, TimePickerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- tt-reservations-frontend/src/app/time-picker/time-picker.component.ts
+++ tt-reservations-frontend/src/app/time-picker/time-picker.component.ts
@@ -1,13 +1,42 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ReactiveFormsModule  } from '@angular/forms';
 
 @Component({
   selector: 'app-time-picker',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './time-picker.component.html',
   styleUrl: './time-picker.component.scss'
 })
-export class TimePickerComponent {
-  day = new FormControl('');
+export class TimePickerComponent implements OnInit {
+  chosen_time: FormGroup = new FormGroup({});
+  
+  ngOnInit(): void {
+    this.chosen_time = new FormGroup({
+      day: new FormControl(this.get_suggested_date()),
+      start_time: new FormControl(this.get_suggested_start_time()),
+      end_time: new FormControl(this.get_suggested_end_time())
+    });    
+  }
+
+  get_suggested_date() {
+    return "2024-09-15"
+  }
+
+  get_suggested_start_time() {
+    return ""
+  }
+
+  get_suggested_end_time() {
+    return ""
+  }
+
+  onSubmit() {
+    if (this.chosen_time.valid) {
+      console.warn(this.chosen_time.value);
+    } else {
+      console.error("Form is invalid");
+    }
+  }
 }
