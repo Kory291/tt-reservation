fcee93dae221342f1635483bbfbe00d278faed30
Kory291 <01lukasschaefer@gmail.com>
2025-01-24 18:29:46 +0100

    update Docker thingies

---
+++ tt-reservations-frontend/entrypoint.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+if [[ $SSL_CERT && $SSL_KEY ]]; then
+    echo "Using SSL"
+    ng serve --host 0.0.0.0 --ssl --ssl-cert $SSL_CERT --ssl-key $SSL_KEY
+else
+    echo "Please use SSL in future"
+    ng serve --host 0.0.0.0
+fi
