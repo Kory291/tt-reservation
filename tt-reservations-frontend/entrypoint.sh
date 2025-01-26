1976798a30355144a5a228b616a6449e278c14db
Kory291 <01lukasschaefer@gmail.com>
2025-01-27 00:54:38 +0100

    fix entrypoint if

--- tt-reservations-frontend/entrypoint.sh
+++ tt-reservations-frontend/entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-if [ -n "$SSL_CERT" ] && [ -n "$SSL_KEY" ]; then
+if [ -z "$SSL_CERT" ] && [ -z "$SSL_KEY" ]; then
     echo "Using SSL"
     ng serve --host 0.0.0.0 --ssl --ssl-cert "$SSL_CERT" --ssl-key "$SSL_KEY"
 else
