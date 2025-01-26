c350647dc4c1271ba6ce120f1c1148b82abf7805
Kory291 <01lukasschaefer@gmail.com>
2025-01-27 00:27:42 +0100

    change frontend image to use sh instead of bash

--- tt-reservations-frontend/entrypoint.sh
+++ tt-reservations-frontend/entrypoint.sh
@@ -1,8 +1,8 @@
-#! /bin/bash
+#! /bin/sh
 
-if [[ $SSL_CERT && $SSL_KEY ]]; then
+if [ -n "$SSL_CERT" ] && [ -n "$SSL_KEY" ]; then
     echo "Using SSL"
-    ng serve --host 0.0.0.0 --ssl --ssl-cert $SSL_CERT --ssl-key $SSL_KEY
+    ng serve --host 0.0.0.0 --ssl --ssl-cert "$SSL_CERT" --ssl-key "$SSL_KEY"
 else
     echo "Please use SSL in future"
     ng serve --host 0.0.0.0
